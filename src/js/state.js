5cf572f3f1125f4c8aae9192feed2b88542af2d9
Guilherme Costa <guilherme.costa.local>
2018-07-10 22:28:49 -0300

    task ### WEB-120 done

--- src/js/state.js
+++ src/js/state.js
@@ -1,43 +1,53 @@
 import {createStore} from './lib/state';
+import {addTodoItem, editTodoItem, listTodos} from './lib/feature';
 
-const initialState = {
-    todos: [
-        {
-            id: 0,
-            text: 'Take a look at the application',
-            done: true
-        },
-        {
-            id: 1,
-            text: 'Add ability to filter todos',
-            done: false
-        },
-        {
-            id: 2,
-            text: 'Filter todos by status',
-            done: false
-        },
-        {
-            id: 3,
-            text: 'Filter todos by text',
-            done: false
-        }
-    ]
+
+// const initialState = {
+//     todos: [
+//         {
+//             id: 0,
+//             text: 'Take a look at the application',
+//             done: true
+//         },
+//         {
+//             id: 1,
+//             text: 'Add ability to filter todos',
+//             done: false
+//         },
+//         {
+//             id: 2,
+//             text: 'Filter todos by status',
+//             done: false
+//         },
+//         {
+//             id: 3,
+//             text: 'Filter todos by text',
+//             done: false
+//         }
+//     ]
+// };
+
+let initialState = {
+    todos: listTodos()
 };
 
 function todoChangeHandler(state, change) {
     switch(change.type) {
         case 'ADD_TODO':
-            state.todos.push({
+            let obj = {
                 id: state.todos.length,
                 text: change.text,
                 done: false
-            });
+            };
+            state.todos.push(obj);
+            addTodoItem(obj);
+
             break;
         case 'TODO_TOGGLE_DONE':
             for(let todo of state.todos) {
                 if(todo.id === change.id) {
                     todo.done = !todo.done;
+                    editTodoItem(todo, todo.done);
                     break;
                 }
             }
