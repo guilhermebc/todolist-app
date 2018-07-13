4684de6fdd898ea9844c88fcdad98be74879a753
Guilherme Costa <guilherme.costa.local>
2018-07-12 21:20:28 -0300

    ajusts

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
-import '../css/main.css';
-
 import {todos} from './state';
 import {render} from './view';
 import {registerEventHandlers} from './events';
+
 
 todos.subscribe(newState => render(document.body, newState));
 
